01aafac396c4d3699c8cf92099501f79c805fe95
Liu6625 <1411077424@qq.com>
2020-07-27 15:14:47 +0800

    add header

---
+++ src/components/home/pages/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class='iconfont'>
+				&#xe624;
+			</span>
+        </div>
+        <div class="header-search">
+            <span class="iconfont">
+                &#xe632;
+            </span>
+            输入城市/景点/游玩主题
+            <!-- <input type="text" placeholder="输入城市/景点/游玩主题"> -->
+        </div>
+        <div class="header-right">
+            北京
+            <span class="iconfont">
+                &#xe6aa;
+            </span>
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style scoped lang="stylus">
+    @import '~css/var.styl';
+    /* 在style中加，在js中不用加 */
+    
+    .header {
+        display: flex;
+        width: 100%;
+        line-height: .88rem;
+        height: 0.88rem;
+        background: $bgColor;
+        color: $textColor;
+        font-size: .36rem;
+    }
+    
+    .header-left {
+        width: .4rem;
+        padding: 0 .2rem;
+        text-align: center;
+        font-weight: bold;
+    }
+    
+    .header-search {
+        flex: 1;
+        background: #fff;
+        height: .6rem;
+        margin: .14rem 0;
+        border-radius: .1rem;
+        color: #e4e7ea;
+        line-height: .6rem;
+        font-size: .28rem;
+        padding-left: .2rem;
+    }
+    
+    .header-right {
+        font-size: .28rem;
+        padding: 0 .2rem;
+    }
+    
+    .header-right a {
+        color: #fff;
+    }
+</style>
