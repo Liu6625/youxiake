b81f5e07eb1343639e858daa85003116ea76d5ff
Liu6625 <1411077424@qq.com>
2020-07-31 10:02:19 +0800

    add location

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,17 +3,20 @@
         <Home-Header></Home-Header>
         <Home-Swiper></Home-Swiper>
         <Home-Icons></Home-Icons>
+        <Home-location></Home-location>
     </div>
 </template>
 <script>
     import HomeHeader from './pages/Header'
     import HomeSwiper from './pages/Swiper'
     import HomeIcons from './pages/Icons'
+    import HomeLocation from './pages/Location'
     export default {
         components: {
             HomeHeader,
             HomeSwiper,
-            HomeIcons
+            HomeIcons,
+            HomeLocation
         }
     }
 </script>
---
+++ src/components/home/pages/Location.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="location border-top">
+        <div class="location-item border-right">
+            <span class="iconfont">&#xe63f;</span>
+            湖南长沙
+        </div>
+        <div class="location-item">
+            <span class="iconfont">&#xe689;</span>
+            玩水季
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+    .location {
+        position: relative;
+        display: flex;
+        background: #fff;
+    }
+    
+    .location .location-item {
+        flex: 1;
+        font-size: .28rem;
+        text-align: center;
+        line-height: 1rem;
+    }
+</style>
