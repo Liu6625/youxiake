b455137eefc9c3abe60d119149710d7ff7713ea4
Liu6625 <1411077424@qq.com>
2020-08-02 11:09:54 +0800

    add city-list

---
+++ src/components/city/pages/List.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="list">
+        <div>
+            <div class="list-title">A</div>
+            <ul class="list-msg">
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿被速度快发送
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+                <li class="list-item">
+                    阿克苏地区
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .list-title {
+        font-size: .24rem;
+        color: #212121;
+        padding: .2rem .3rem;
+    }
+    
+    .list-msg {
+        position: relative;
+        background: #fff;
+        overflow: hidden;
+    }
+    
+    .list-msg::before {
+        content: '';
+        position: absolute;
+        left: 25%;
+        width: 25%;
+        height: 100%;
+        border-left: .02rem solid #ddd;
+        border-right: .02rem solid #ddd;
+    }
+    
+    .list-msg::after {
+        content: '';
+        position: absolute;
+        left: 75%;
+        width: 75%;
+        height: 100%;
+        border-left: .02rem solid #ddd;
+    }
+    
+    .list-item {
+        float: left;
+        width: 25%;
+        line-height: 0.9rem;
+        font-size: .28rem;
+        text-align: center;
+        border-bottom: .02rem solid #ddd;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+</style>
