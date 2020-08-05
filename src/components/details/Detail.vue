bc2a57589a2a8e5c9fced77d571ff1b9bee89681
Liu6625 <1411077424@qq.com>
2020-08-05 11:09:40 +0800

    detail-header

--- src/components/details/Detail.vue
+++ src/components/details/Detail.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <detail-Banner></detail-Banner>
+        <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div style="height: 3000px;"></div>
     </div>
 </template>、
 <script>
     import DetailBanner from './pages/Banner'
+    import DetailHeader from './pages/Header'
     export default {
         components: {
-            DetailBanner
+            DetailBanner,
+            DetailHeader
         }
     }
 </script>
